2f1d0f32f8309ad0b2dc8a63be04be18a0d342ef
Vikas Pritam <pritam.developer.1@gmail.com>
2024-12-25 18:23:36 +0530

    learned about file systems and readline modules

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const Person = require('./Person')
+const fs = require("fs").promises;
 
-const person1 = new Person('Vikas', 18);
+async function readJSONFile(filename) {
+  try {
+    const data = await fs.readFile(filename, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error reading ${filename}: ${error}`);
+    return [];
+  }
+}
 
-person1.on('called', ()=>{
-    console.log(`My name is ${person1.name} and I'm ${person1.age}`);
-});
-person1.emit('called');
+async function main() {
+  try {
+    const names = await readJSONFile("names.json");
+    const addresses = await readJSONFile("address.json");
+
+    const bioData = names.map((name) => {
+      const matchingAddress = addresses.find(
+        (address) => address.id === name.id
+      );
+      return { ...name, ...matchingAddress };
+    });
+
+    await fs.writeFile("bio.json", JSON.stringify(bioData, null, 2));
+    console.log("bio.json created successfully!");
+  } catch (error) {
+    console.error("Error combining data:", error);
+  }
+}
+main();
